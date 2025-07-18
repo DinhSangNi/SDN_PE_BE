7319962143426012521df02bfd0f0ac370529293
TruongDinhSang <sangprotqb@gmail.com>
2025-07-19 01:38:06 +0700

    set up source base backend

---
+++ src/app.module.ts
@@ -0,0 +1,35 @@
+import { Logger, Module, OnModuleInit } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { InjectConnection, MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Connection } from 'mongoose';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements OnModuleInit {
+  private readonly logger = new Logger(AppModule.name);
+
+  constructor(@InjectConnection() private readonly connection: Connection) {}
+
+  async onModuleInit() {
+    this.connection
+      .asPromise()
+      .then(() => this.logger.log('✅ Connected to MongoDB!'))
+      .catch((err) => this.logger.error('❌ MongoDB connection error:', err));
+  }
+}
